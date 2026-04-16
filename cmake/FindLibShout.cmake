72771db973b6cd1fb90d0d54b9b88ad1f2d9293c
Aaron Cupp <mrcupp@mrcupp.com>
2026-04-15 22:16:07 -0700

    build: add FindLibShout.cmake module

---
+++ cmake/FindLibShout.cmake
@@ -0,0 +1,32 @@
+find_path(LibShout_INCLUDE_DIR
+  NAMES shout/shout.h
+  PATHS
+    /usr/include
+    /usr/local/include
+    /opt/homebrew/include
+    ${LibShout_ROOT}/include
+    $ENV{LIBSHOUT_INCLUDE}
+)
+
+find_library(LibShout_LIBRARY
+  NAMES shout libshout
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/homebrew/lib
+    ${LibShout_ROOT}/lib
+    $ENV{LIBSHOUT_LIB}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibShout
+  REQUIRED_VARS LibShout_LIBRARY LibShout_INCLUDE_DIR
+)
+
+if(LibShout_FOUND AND NOT TARGET LibShout::LibShout)
+  add_library(LibShout::LibShout UNKNOWN IMPORTED)
+  set_target_properties(LibShout::LibShout PROPERTIES
+    IMPORTED_LOCATION "${LibShout_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${LibShout_INCLUDE_DIR}"
+  )
+endif()
